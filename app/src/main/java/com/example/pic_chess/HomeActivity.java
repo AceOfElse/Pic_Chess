d1c49e22fda0642f28f023cc98dd2058c047c781
tan_nguyen <nguyenthanhtan091002@outlook.com>
2022-04-08 21:00:03 -0500

    Updated at 9:00PM April 8 by Tan

--- app/src/main/java/com/example/pic_chess/HomeActivity.java
+++ app/src/main/java/com/example/pic_chess/HomeActivity.java
@@ -2,15 +2,19 @@ package com.example.pic_chess;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
+import android.widget.ImageButton;
 import android.widget.ImageView;
 import android.widget.TextView;
 
 public class HomeActivity extends AppCompatActivity {
     private TextView titleText;
     private ImageView chessPreviewImage, drawPreviewImage;
-    private Button chessPopUpButton, drawPopUpButton, settingButton;
+    //private Button chessPopUpButton, drawPopUpButton;
+    private ImageButton settingButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,8 +24,20 @@ public class HomeActivity extends AppCompatActivity {
         titleText = findViewById(R.id.titleText);
         chessPreviewImage = findViewById(R.id.chessPreviewImage);
         drawPreviewImage = findViewById(R.id.drawPreviewImage);
-        chessPopUpButton = findViewById(R.id.chessPopUpButton);
-        drawPopUpButton = findViewById(R.id.drawPopUpButton);
+        //chessPopUpButton = findViewById(R.id.chessPopUpButton);
+        //drawPopUpButton = findViewById(R.id.drawPopUpButton);
         settingButton = findViewById(R.id.settingButton);
+
+        chessPreviewImage.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                openPvp();
+            }
+        });
+    }
+
+    public void openPvp() {
+        Intent pvpIntent = new Intent(HomeActivity.this, PvpChessActivity.class);
+        startActivity(pvpIntent);
     }
 }
--- app/src/main/java/com/example/pic_chess/PvpChessActivity.java
+++ app/src/main/java/com/example/pic_chess/PvpChessActivity.java
@@ -1,8 +1,12 @@
 package com.example.pic_chess;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.constraintlayout.widget.ConstraintLayout;
+import androidx.constraintlayout.widget.ConstraintSet;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.ImageButton;
 import android.widget.TableLayout;
@@ -11,7 +15,7 @@ import android.widget.TextView;
 public class PvpChessActivity extends AppCompatActivity {
     private ImageButton backButton, newGameButton, endButton, resignButton;
     private TextView timerText;
-    private TableLayout boardLayout;
+    private ConstraintLayout boardLayout, pieceLayout;
 
 
     @Override
@@ -24,5 +28,24 @@ public class PvpChessActivity extends AppCompatActivity {
         endButton = findViewById(R.id.endButton);
         resignButton = findViewById(R.id.resignButton);
         timerText = findViewById(R.id.timerText);
+        boardLayout = findViewById(R.id.boardLayout);
+        pieceLayout = findViewById(R.id.pieceLayout);
+
+        //Set listeners
+        backButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                returnHome();
+            }
+        });
+    }
+
+    public void returnHome() {
+        Intent homeIntent = new Intent(PvpChessActivity.this, HomeActivity.class);
+        startActivity(homeIntent);
+    }
+
+    private void promptPanel() {
+
     }
 }
