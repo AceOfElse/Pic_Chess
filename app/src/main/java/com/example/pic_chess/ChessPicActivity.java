cfec22ecbf8bed4b94d79b5847a3fce40b0c47d0
tan_nguyen <nguyenthanhtan091002@outlook.com>
2022-04-15 17:46:23 -0500

    Adding AlertDialogue popup menu for HomeActivity and ChessPicActivity. Updating on the layout of ChessPicActivity

--- app/src/main/java/com/example/pic_chess/ChessPicActivity.java
+++ app/src/main/java/com/example/pic_chess/ChessPicActivity.java
@@ -2,32 +2,80 @@ package com.example.pic_chess;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.AlertDialog;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.ImageButton;
+import android.widget.ToggleButton;
 
 public class ChessPicActivity extends AppCompatActivity {
-    private ImageButton backButton;
+    private ImageButton backButton, newCanvasButton, loadFileButton, saveFileButton, submitFileButton;
+    private ToggleButton toolbarButton;
+    private AlertDialog.Builder alertDialogue;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_chess_pic);
+        //Set up alert dialogue
+        alertDialogue = new AlertDialog.Builder(ChessPicActivity.this);
 
+        //Find views
         backButton = findViewById(R.id.backButtonCP);
+        newCanvasButton = findViewById(R.id.newCanvasButtonCP);
+        loadFileButton = findViewById(R.id.loadFileButtonCP);
+        saveFileButton = findViewById(R.id.saveFileCP);
+        submitFileButton = findViewById(R.id.submitFileCP);
+        toolbarButton = findViewById(R.id.toolbarButtonCP);
 
         //Set button listeners
         backButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                returnHome();
+                onClickShowAlert(view);
             }
         });
     }
 
-    private void returnHome() {
-        Intent homeIntent = new Intent(ChessPicActivity.this, HomeActivity.class);
-        startActivity(homeIntent);
+    //Set activity life cycles
+    protected void onStart() {
+        super.onStart();
+    }
+
+    protected void onRestart() {
+        super.onRestart();
+    }
+
+    protected void onPause() {
+        super.onPause();
+    }
+
+    protected void onResume() {
+        super.onResume();
+    }
+
+    protected void onStop() {
+        super.onStop();
+    }
+
+    private void onClickShowAlert(View view) {
+        alertDialogue.setMessage(R.string.prompt_back_text);
+        alertDialogue.setPositiveButton("YES", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialogInterface, int i) {
+                dialogInterface.dismiss();
+                finish();
+            }
+        });
+        alertDialogue.setNegativeButton("NO", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialogInterface, int i) {
+                dialogInterface.dismiss();
+            }
+        });
+        alertDialogue.create();
+        alertDialogue.show();
     }
 }
--- app/src/main/java/com/example/pic_chess/HomeActivity.java
+++ app/src/main/java/com/example/pic_chess/HomeActivity.java
@@ -2,6 +2,8 @@ package com.example.pic_chess;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.AlertDialog;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
@@ -11,12 +13,15 @@ import android.widget.TextView;
 public class HomeActivity extends AppCompatActivity {
     private TextView titleText;
     private ImageButton settingButton, chessPreviewButton, drawPreviewButton;
+    private AlertDialog.Builder alertDialogue;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+        alertDialogue = new AlertDialog.Builder(HomeActivity.this);
 
+        //Find views
         titleText = findViewById(R.id.titleText);
         chessPreviewButton = findViewById(R.id.chessPreviewButton);
         drawPreviewButton = findViewById(R.id.drawPreviewButton);
@@ -35,6 +40,46 @@ public class HomeActivity extends AppCompatActivity {
                 openChessPic();
             }
         });
+    }
+
+    //Set activity life cycles
+    protected void onStart() {
+        super.onStart();
+    }
+
+    protected void onRestart() {
+        super.onRestart();
+    }
+
+    protected void onPause() {
+        super.onPause();
+    }
+
+    protected void onResume() {
+        super.onResume();
+    }
+
+    protected void onStop() {
+        super.onStop();
+    }
+
+    public void onBackPressed() {
+        alertDialogue.setMessage(R.string.prompt_quit_text);
+        alertDialogue.setPositiveButton("YES", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialogInterface, int i) {
+                dialogInterface.dismiss();
+                finishAffinity();
+            }
+        });
+        alertDialogue.setNegativeButton("NO", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialogInterface, int i) {
+                dialogInterface.dismiss();
+            }
+        });
+        alertDialogue.create();
+        alertDialogue.show();
     }
 
     private void openPvp() {
